caf6cd3fc80d9cd603d57057e2897e957ff694f0
Michael Kitson <michaelskitson@gmail.com>
2020-12-04 21:53:35 -0800

    cleanup

--- 5/5.sh
+++ 5/5.sh
@@ -8,11 +8,8 @@ function sorted {
     echo $((2#$i))
   done | sort -n
 }
-all=`sorted`
 max=`sorted | tail -n 1`
 min=`sorted | head -n 1`
-
+missing=`comm -23 <(seq $min $max) <(sorted)`
 echo "Part 1: $max"
-
-echo -n "Part 2: "
-comm -23 <(seq $min $max) <(sorted)
+echo "Part 2: $missing"
